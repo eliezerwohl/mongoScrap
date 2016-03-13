a5af4588feaac79f1b8e5c6860b6255f280e3c4f
eliezerwohl <eliezerwohl@gmail.com>
2016-03-13 16:47:02 -0400

    beautified the index.js file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -19,18 +20,12 @@ db.on('error', function(err) {
 db.once('open', function() {
   console.log('Mongoose connection successful.');
 });
-
 var Schema = mongoose.Schema;
-
 var Article = new Schema({
   article: String
 });
 
 var Title = mongoose.model('Title', Article);
-
-
-
-
 app.use(express.static('public'));
 var request = require('request');
 var cheerio = require('cheerio');
@@ -40,7 +35,9 @@ app.get('/', function(req, res) {
 });
 
 app.post("/submit", function(req, res) {
-  var article = new Title({article:req.body.article})
+  var article = new Title({
+    article: req.body.article
+  })
 
   article.save(function(err, doc) {
     if (err) {
@@ -54,35 +51,37 @@ app.post("/submit", function(req, res) {
 
 
 
-app.get("/huffscrape", function(req, res){
-  request('http://www.huffingtonpost.com/', function (error, response, body) {
-  var results = [];
-  if (!error && response.statusCode == 200) {
-    $ = cheerio.load(body);
+app.get("/huffscrape", function(req, res) {
+  request('http://www.huffingtonpost.com/', function(error, response, body) {
+    var results = [];
+    if (!error && response.statusCode == 200) {
+      $ = cheerio.load(body);
 
-    $('h2').each(function(i, elem){
-     var theTitle = $(this).children("a").text();
-     console.log(theTitle)
-        var Article = new Title({article: theTitle});
-      Article.save(function(err, doc) {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log("saved");
-      
+      $('h2').each(function(i, elem) {
+        var theTitle = $(this).children("a").text();
+        console.log(theTitle)
+        var Article = new Title({
+          article: theTitle
+        });
+        Article.save(function(err, doc) {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log("saved");
+
+          }
+        });
+        results.push({
+          title: $(this).children("a").text(),
+          // link: "http://www.reddit.com" + $(this).children().attr("href")
+        })
+      });
     }
-  });
-  results.push({
-        title: $(this).children("a").text(),
-        // link: "http://www.reddit.com" + $(this).children().attr("href")
-      })
-    });
-  }
 
-})
+  })
   res.send("complete")
 })
 
 app.listen(PORT, function() {
-  console.log('App running on port '+ PORT);
+  console.log('App running on port ' + PORT);
 });
