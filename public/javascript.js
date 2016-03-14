38f66565f9c9c3ee23db84d1784ef85f68b924fc
eliezerwohl <eliezerwohl@gmail.com>
2016-03-13 21:45:45 -0400

    added a way for each form to contain the correct id so review may be added and stored with the correct headline

--- public/javascript.js
+++ public/javascript.js
@@ -5,7 +5,13 @@ $.getJSON("/all", function(data){
 console.log(data[10])
 dataNum = 10;
 var oneData = data[dataNum];
+var objectId =  oneData._id;
+
 $("#headline").append(oneData.article)
+$("#review").append('<form action="/review' + objectId + '" method="post">\
+      <textarea rows="4" cols="50">  </textarea>\
+      <button type="submit"> Comment on this headline </button>\
+    </form>')
 })
 
 
@@ -13,12 +19,25 @@ $("#next").on("click", function(){
 $.getJSON("/all", function(data){
 dataNum +=1;
 var oneData = data[dataNum];
-$("#headline").empty()
-
-$("#headline").append(oneData.article)
+$("#headline, #review").empty()
+var objectId =  oneData._id;
+$("#headline").append(oneData.article);
+$("#review").append('<form action="/review' + objectId + '" method="post">\
+      <textarea rows="4" cols="50">  </textarea>\
+      <button type="submit"> Comment on this headline </button>\
+    </form>')
 })
 
 })
+
+// <form action="/submit" method="post">
+    
+//       <textarea rows="4" cols="50">  </textarea>
+//       <button type="submit"> Comment on this headline </button>
+//     </form>
+
+
+// <form action="/review/objectId" method="post">
 // psuedo code.
 
 // display
