bf03987efbff31e8ffeaf4a05aafd3386cb1b08f
eliezerwohl <eliezerwohl@gmail.com>
2016-03-13 20:46:33 -0400

    added button which render the next headline from the mdb

--- public/javascript.js
+++ public/javascript.js
@@ -1,11 +1,28 @@
+var dataNum;
+
+
 $.getJSON("/all", function(data){
 console.log(data[10])
+dataNum = 10;
+var oneData = data[dataNum];
+$("#headline").append(oneData.article)
 })
 
-psuedo code.
 
-display
+$("#next").on("click", function(){
+$.getJSON("/all", function(data){
+dataNum +=1;
+var oneData = data[dataNum];
+$("#headline").empty()
 
-store as a var
-when press button, +1 to the var
+$("#headline").append(oneData.article)
+})
+
+})
+// psuedo code.
+
+// display
+
+// store as a var
+// when press button, +1 to the var
 
